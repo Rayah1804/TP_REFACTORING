df2b4cf765114d07ac91c8e6560ceefcf0b5a764
Rayah1804 <rajaonahasina1804@gmail.com>
2024-04-02 09:10:35 +0200

    Création de la classe Humain

--- TP REFACTORING/src/iut/bad/Femme.java
+++ TP REFACTORING/src/iut/bad/Femme.java
@@ -1,16 +1,15 @@
 package iut.bad;
 
-public class Femme {
-    private String nom;
-    private String prenom;
-    private int age;
-
- 
-    public Femme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
+public class Femme extends Humain {
+    
+    public Femme() {
+        super(); 
     }
 
     
+    public Femme(String nom, String prenom, int age) {
+        super(nom, prenom, age); 
+    }
+    
+   
 }
