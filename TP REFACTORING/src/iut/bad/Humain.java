832bad31127341e5f320b2293b950289e9f64069
Rayah1804 <rajaonahasina1804@gmail.com>
2024-04-02 09:15:12 +0200

    ajouter une méthode details dans la classe Humain pour afficher le nom, prénom et âge

--- TP REFACTORING/src/iut/bad/Humain.java
+++ TP REFACTORING/src/iut/bad/Humain.java
@@ -1,16 +1,15 @@
 package iut.bad;
 
 public class Humain {
-    
     protected String nom;
     protected String prenom;
     protected int age;
     
-    
+   
     public Humain() {
     }
     
-  
+    
     public Humain(String nom, String prenom, int age) {
         this.nom = nom;
         this.prenom = prenom;
@@ -18,4 +17,9 @@ public class Humain {
     }
     
     
+    public void details() {
+        System.out.println("Nom: " + nom);
+        System.out.println("Prénom: " + prenom);
+        System.out.println("Âge: " + age);
+    }
 }
