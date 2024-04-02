5a2d40f58cdd96a2e420c0f2a191d68d75346960
Rayah1804 <rajaonahasina1804@gmail.com>
2024-04-02 09:44:50 +0200

    creation une méthode main() dans la classe Femme pour instancier un Homme et une Femme

--- TP REFACTORING/src/iut/bad/Femme.java
+++ TP REFACTORING/src/iut/bad/Femme.java
@@ -1,15 +1,29 @@
 package iut.bad;
 
 public class Femme extends Humain {
-    
+    private String nom;
+    private String prenom;
+    private int age;
+
+   
     public Femme() {
-        super(); 
     }
 
     
     public Femme(String nom, String prenom, int age) {
-        super(nom, prenom, age); 
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
     }
+
     
-   
+    public static void main(String[] args) {
+        
+        Homme homme = new Homme("Doe", "John", 30);
+        Femme femme = new Femme("Doe", "Jane", 28);
+
+        
+       homme.ami(femme);
+       femme.ami(homme);
+    }
 }
