7f5b694b674cf0a00095abe0408d80b71d1115a2
Rayah1804 <rajaonahasina1804@gmail.com>
2024-04-02 09:27:48 +0200

    Ajout des méthodes manger() et boire() à la classe Humain

--- TP REFACTORING/src/iut/bad/Humain.java
+++ TP REFACTORING/src/iut/bad/Humain.java
@@ -20,8 +20,16 @@ public class Humain {
     public String toString() {
         return "Nom: " + nom + "\nPrénom: " + prenom + "\nÂge: " + age;
     }
+    public void manger() {
+        System.out.println("Je suis en train de manger.");
+    }
+
+    
+    public void boire() {
+        System.out.println("Je suis en train de boire.");
+    }
 }
-//Si on décide de déplacer la méthode `toString()` vers les sous-classes `Homme` et `Femme`,
+// QUESTIONS 9)si on décide de déplacer la méthode `toString()` vers les sous-classes `Homme` et `Femme`,
 //chaque sous-classe aurait sa propre implémentation de la méthode `toString()`.
 // Cela peut être approprié si chaque sous-classe a des détails spécifiques à afficher.
 // Cependant, si les détails à afficher sont les mêmes pour toutes les sous-classes
